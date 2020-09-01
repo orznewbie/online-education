1128507af878b1bcdf2d62fd63a4465d335255b4
orznewbie <orznewbie@gmail.com>
2020-09-01 23:37:19 +0800

    AAA

--- Test.java
+++ Test.java
@@ -1,9 +1,4 @@
 public class Test {
   public static void main(String[] args) {
-
-    System.out.println("git-test");
-    System.out.println("git-test1");
-    System.out.println("git-test1111");
-    System.out.println("git-test11");
   }
 }
